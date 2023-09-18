7303ba3ddfa1bf48abbc4e78dbf369c837a75b96
XuanNT1910 <132054422+XuanNT1910@users.noreply.github.com>
2023-09-18 15:24:48 +0700

    array36933

--- src/array/Array36933.java
+++ src/array/Array36933.java
@@ -13,9 +13,33 @@ public class Array36933 {
         }
         Scanner sc = new Scanner(System.in);
         int tc = sc.nextInt();
-        char[][] a = new char[500][500];
-        int m,n;
-        m = sc.nextInt();
-        n = sc.nextInt();
+        for (int t = 1; t <= tc; t++) {
+            int m, n;
+            m = sc.nextInt();
+            n = sc.nextInt();
+            sc.nextLine();
+            char[][] a = new char[5 * m + 1][5 * n + 1];
+            for (int j = 0; j < 5 * m + 1; j++) {
+                String s = sc.nextLine();
+                a[j] = s.toCharArray();
+            }
+            int[] res = new int[5];
+            for (int i = 1; i < 5 * m + 1; i += 5) {
+                for (int j = 1; j < 5 * n + 1; j += 5) {
+                    int count = 0;
+                    for (int k = 0; k < 5; k++) {
+                        if (a[k + i][j] == '*') {
+                            count++;
+                        }
+                    }
+                    res[count]++;
+                }
+            }
+            System.out.print("#" + t + " ");
+            for (int i = 0; i < 5; i++) {
+                System.out.print(res[i] + " ");
+            }
+            System.out.println();
+        }
     }
 }
